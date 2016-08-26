f50a11f63adfb3407dc34a4500a4f713e0b1f2d0
ashley-si <si.jun.ke.107@gmail.com>
2016-08-26 10:51:34 +0800

    Revert "[T2A3][W09-A4]Yeo Xiang Guang Brandon" (#414)

--- src/seedu/addressbook/AddressBook.java
+++ src/seedu/addressbook/AddressBook.java
@@ -200,18 +200,7 @@ public class AddressBook {
      */
     public static void main(String[] args) {
         showWelcomeMessage();
-        if (args.length >= 2) {
-		    showToUser(MESSAGE_INVALID_PROGRAM_ARGS);
-		    exitProgram();
-		}
-		
-		if (args.length == 1) {
-		    setupGivenFileForStorage(args[0]);
-		}
-		
-		if(args.length == 0) {
-		    setupDefaultFileForStorage();
-		}
+        processProgramArgs(args);
         loadDataFromStorage();
         while (true) {
             String userCommand = getUserInput();
